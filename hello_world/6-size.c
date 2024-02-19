bcbad933ed7e1d33d8d8c34351ac8cbb7cafd00a
LucasNGRE <lucas12negre@gmail.com>
2024-02-19 14:16:47 +0000

    size of various type

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	printf("Size of a char : %zu byte(s)\n", sizeof(char));
-	printf("Size of an int : %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long int : %zu byte(s)\n", sizeof(char));
-	printf("Size of a float : %zu byte(s)\n", sizeof(float));
+	printf("Size of a char : %lu byte(s)\n", sizeof(char));
+	printf("Size of an int : %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long int : %lu byte(s)\n", sizeof(char));
+	printf("Size of a float : %lu byte(s)\n", sizeof(float));
 }
