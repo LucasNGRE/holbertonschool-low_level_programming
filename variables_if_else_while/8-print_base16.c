fdfb6f06b7282429d188e3d156c8a518594ef69b
LucasNGRE <lucas12negre@gmail.com>
2024-02-20 07:32:39 -0800

    add a putchar to have base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line.
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l;
+
+	for (l = 48; l <= 57; l++)
+	{
+	putchar(l);
+	}
+	for (l = 97; l <= 101; l++)
+	{
+	putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
