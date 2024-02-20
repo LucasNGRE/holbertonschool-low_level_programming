aa80fb678fbf7229fad827aa9e31884170ca591f
LucasNGRE <lucas12negre@gmail.com>
2024-02-20 01:30:11 -0800

    with no problem with betty

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* main - Entry point */
-
-/* return: 0 (success) */
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
