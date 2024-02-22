ce76dffae7d3c4154150fe8213ae80ac2c0f1526
LucasNGRE <lucas12negre@gmail.com>
2024-02-22 12:04:14 +0000

    print the last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "ctype.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - Entry point
+ * @i: character to be verified
+ * Return:0
+ */
+int print_last_digit(int i)
+{
+	return (i % 10);
+}
