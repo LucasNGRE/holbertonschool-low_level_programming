d52a4112bf470404638fc6c6cc39fc3861888f5e
LucasNGRE <lucas12negre@gmail.com>
2024-02-23 09:33:08 +0000

    add to integers and return the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "ctype.h"
+#include <stdlib.h>
+#include "time.h"
+/**
+ * add - Entry point
+ * @a: character to be verified
+ * @b: character to be verified
+ * Return:0
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+
+}
