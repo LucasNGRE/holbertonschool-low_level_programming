643831be4b006c368103a3e94a21657c43144284
LucasNGRE <lucas12negre@gmail.com>
2024-02-26 08:48:21 +0000

    create a function that mutiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ * @a: character to be verified
+ * @b: character to be verified
+ * Return:0
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
