da1e0dc87146922e28f621cc48a82670581e87a2
LucasNGRE <lucas12negre@gmail.com>
2024-03-01 15:49:42 +0100

    Write a function that compares two strings.

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcmp - Entry point
+ * @s1: character to be verified
+ * @s2: character to be verified
+ * Return:0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int result;
+	
+	result = 0;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] == s2 [i])
+		{
+			continue;
+		}
+		else
+		{
+			result = s1[i] - '\0' - s2[i] + '\0';
+			return (result);
+		}
+	}
+	return (result);
+}
