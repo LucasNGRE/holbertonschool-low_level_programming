c304545d4fcc5c063abd80bf71f56d126ef2e5ff
LucasNGRE <lucas12negre@gmail.com>
2024-03-04 12:34:32 +0100

    Write a function that locates a character in a string.

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,15 +10,11 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i] >= '\0')
 	{
 		if (s[i] == c)
 		{
 			return (&s[i]);
-		}
-		else
-		{
-			return (0);
 		}
 		i++;
 	}
