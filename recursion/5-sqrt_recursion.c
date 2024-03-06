2ca2abe82577898d1c837e9d10fcade67cd19916
LucasNGRE <lucas12negre@gmail.com>
2024-03-06 11:10:38 +0100

    Write a function that returns the natural square root of a number.

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
-   * racine - Affiche une chaîne
-   * de caractères suivie d'un saut de ligne.
-   * @n: La chaîne de caractères à afficher.
-   * @n: La chaine de caractère à vérifier.
-   * Return: The length of the string
-   */
+ * racine - Affiche une chaîne
+ * de caractères suivie d'un saut de ligne.
+ * @n: La chaîne de caractères à afficher.
+ * @n: La chaine de caractère à vérifier.
+ * Return: The length of the string
+ */
 int racine(int n, int i)
-  {
-          if (i * i == n)
-                  return i;
-          if (i * i > n)
-                  return -1;
-          return (racine(n, i + 1));
-  }
- /**                                                                                                            * _sqrt_recursion - Affiche une chaîne
-   * de caractères suivie d'un saut de ligne.
-   * @n: La chaîne de caractères à afficher.
-   * Return: The length of the string
-   */
+{
+	if (i * i == n)
+		return i;
+	if (i * i > n)
+		return -1;
+	return (racine(n, i + 1));
+}
+/**                                                                                                            * _sqrt_recursion - Affiche une chaîne
+ * de caractères suivie d'un saut de ligne.
+ * @n: La chaîne de caractères à afficher.
+ * Return: The length of the string
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
