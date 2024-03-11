12af27d94ee0434618b8b4d8fb0f18fe1dc95230
LucasNGRE <lucas12negre@gmail.com>
2024-03-11 12:07:34 +0100

    first version of creating a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - Crée un pointeur vers une noiuvelle chaine qui est une copie de str.
+ * @str: La chaine a dupliquer.
+ *
+ * Retourne: Un pointeur sur la chaine dupliquée.
+ */
+int taille(char *str)
+{
+	unsigned int size = 0;
+
+	while (*str != '\0')
+          {
+                  size++;
+                  str++;
+          }
+	return (size);
+}
+
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int size;
+	unsigned int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	size = taille(str);
+
+	dup = malloc((size + 1) * sizeof(char));
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		dup[i] = str[i];
+	}
+	dup[size] = '\0';
+	return (dup);
+}
