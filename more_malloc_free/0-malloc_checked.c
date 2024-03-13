268b12b7ba346ae4525969cc30cfae480e9a584c
LucasNGRE <lucas12negre@gmail.com>
2024-03-13 09:06:26 +0100

    update of task 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -4,12 +4,14 @@
  * malloc_checked - Alloue de la mémoire en utilisant malloc.
  * @b: la taille de mémoire à allouer.
  * Return: Un pointeur vers la mémoire allouée.
- * */
+ **/
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
-	if (ptr == NULL) {
+
+	if (ptr == NULL)
+	{
 		exit(98);
 	}
-	return ptr;
+	return (ptr);
 }
