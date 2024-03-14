d483e20293120e341517f0d3856afa7ac6c4b893
LucasNGRE <lucas12negre@gmail.com>
2024-03-14 14:33:07 +0100

    update task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,6 +16,11 @@ char *str_concat(char *s1, char *s2)
 	int length1 = 0;
 	int length2 = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[length1] != '\0')
 	{
 		length1++;
