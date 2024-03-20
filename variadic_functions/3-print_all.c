681ee21c2cc9f18703941450b344c78bc8a621cc
LucasNGRE <lucas12negre@gmail.com>
2024-03-20 14:14:55 +0100

    Write a function that prints anything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,57 @@
+#include "variadic_functions.h"
+/**
+ * print_all - Print formatted output based on the given
+ * format string and arguments
+ *
+ * @format: A pointer to a constant format string specifying
+ * the types of arguments to be printed.
+ * The format string can contain characters 'c', 'i', 'f', 's'.
+ * Each character corresponds to a specific type of argument:
+ * - 'c': char
+ * - 'i': integer
+ * - 'f': float
+ * - 's': char * (if the string is NULL, "(nil)" is printed instead)
+ * Any other character in the format string is ignored.
+ * The function prints the arguments according to the provided format string.
+ * If the format string is empty or NULL, the function does nothing.
+ *
+ * Return: None
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char *s = NULL;
+	const char *ptr;
+
+	va_start(args, format);
+	ptr = format;
+	while (*ptr != '\0')
+	{
+		switch (*ptr)
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				printf("%s", (s != NULL) ? s : "(nil)");
+				break;
+			default:
+				break;
+		}
+		if (*(ptr + 1) != '\0' && (*(ptr + 1) == 'c' || *(ptr + 1) == 'i' ||
+					*(ptr + 1) == 'f' || *(ptr + 1) == 's'))
+			printf(", ");
+		ptr++;
+	}
+	va_end(args);
+	printf("\n");
+}
+
