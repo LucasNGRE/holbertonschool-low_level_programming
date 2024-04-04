3a342e6add665d98d19e84760593c3d44b9d2e85
LucasNGRE <lucas12negre@gmail.com>
2024-04-04 09:53:39 +0200

    modify the task that we have to create a function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * create_file - Creates a file with the specified name and writes text content to it.
+ * create_file - Creates a file with the specified name 
+ * and writes text content to it.
  * @filename: The name of the file to create.
  * @text_content: A NULL terminated string to write to the file.
  *
@@ -8,19 +9,15 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd = -1;
+	int fd;
 	char *ptr;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-	{
-		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-		if (fd == -1)
-			return (-1);
-		close(fd);
-		return (1);
-	}
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 	if (text_content != NULL)
 	{
 		ptr = text_content;
@@ -33,6 +30,11 @@ int create_file(const char *filename, char *text_content)
 			}
 			ptr++;
 		}
+	}
+	else
+	{
+		close(fd);
+		return (-1);
 	}
 	close(fd);
 	return (1);
